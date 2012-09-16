0c54270186ad9632f0905a2406460abc018ef09e
Your Name <jstevekaranja@gmail.com>
2012-09-16 14:20:40 +0300

    adding contact page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Johnk::Application.routes.draw do
 
   get "static_pages/about"
 
+  get "static_pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
