ea69cad1f38a8c2d4bac735f247eb3ef08da01fb
Your Name <jstevekaranja@gmail.com>
2012-09-16 09:22:53 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Johnk::Application.config.secret_token = '6c7c890c8deeb1229fe07d3cc2760e2c7c141e294896a62d8f8b33114314ff35787a4f46dd8bd7661de965731a2b4d46dbe0bb748d903b4e58b542510b97eb46'
